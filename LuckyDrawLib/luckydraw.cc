6081f21abadf7d07a4575b5b5b29280ed70e93a3
Livia Zhu <livia.zhu@live.com>
2020-05-22 13:49:04 +0800

    implemented without while

--- LuckyDrawLib/luckydraw.cc
+++ LuckyDrawLib/luckydraw.cc
@@ -4,22 +4,30 @@
 #include "luckydraw.hh"
 
 unsigned int* already_drawn = new unsigned int[100];
+int counter = 0;
 
 unsigned int lucky_draw(unsigned int* const source_people)
 {
     srand((unsigned) time(0));
 
-    int index_drawn;
-    do {
-        index_drawn = rand() % 100;
-    } while(already_drawn[index_drawn]);
+    int index_drawn = rand()%(100-counter);
+    int add = 0;
+    for (int i = 0; i < counter; i++){
+        if (index_drawn >= *(already_drawn + i)) {
+            add++;
+        }
+    }
 
-    *(already_drawn + index_drawn) = 1; 
-    return *(source_people + index_drawn);
+    int actual_index = index_drawn + add;
+    already_drawn[counter] = actual_index; 
+    counter++;
+
+    return *(source_people + actual_index);
 }
 
 void free_already_drawn()
 {
     delete[] already_drawn;
     already_drawn = 0;
+    counter = 0;
 }
